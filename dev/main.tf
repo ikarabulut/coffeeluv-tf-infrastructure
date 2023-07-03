a28c7ed377dce7111191b641b145f27786cecc1f
Ismail Karabulut <i.karabulut93@gmail.com>
2023-07-02 19:50:45 -0500

    add remote backend and environments

--- dev/main.tf
+++ dev/main.tf
@@ -1,4 +1,13 @@
 terraform {
+  backend "s3" {
+    bucket         = "coffeeluv-tf-backend"
+    key            = "dev/coffeeluv-infra/terraform.tfstate"
+    region         = "us-east-2"
+
+    dynamodb_table = "coffeeluv-locks"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
